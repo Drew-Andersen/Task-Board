6698c7921f4d0e7345d837dae3c08494489c63cd
Drew-Andersen <andrewtandersen@gmail.com>
2024-07-07 16:22:46 -0400

    Added the necessary folders and files. Also added the modal form.

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+// Added the JavaScript for the Bootstrap modal from
+var formModal = $('formModal')
+formModal.on('show.bs.modal', function (event) {
+  // Button that triggered the modal
+  var button = event.relatedTarget
+  // Extract info from data-bs-* attributes
+  var recipient = button.getAttribute('data-bs-whatever')
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  var modalTitle = formModal.querySelector('.modal-title')
+  var modalBodyInput = formModal.querySelector('.modal-body input')
+
+  modalBodyInput.value = recipient
+})
+
+
+
+
+// Retrieve tasks and nextId from localStorage
+let taskList = JSON.parse(localStorage.getItem("tasks"));
+let nextId = JSON.parse(localStorage.getItem("nextId"));
+
+// Todo: create a function to generate a unique task id
+function generateTaskId() {
+
+}
+
+// Todo: create a function to create a task card
+function createTaskCard(task) {
+
+}
+
+// Todo: create a function to render the task list and make cards draggable
+function renderTaskList() {
+
+}
+
+// Todo: create a function to handle adding a new task
+function handleAddTask(event){
+
+}
+
+// Todo: create a function to handle deleting a task
+function handleDeleteTask(event){
+
+}
+
+// Todo: create a function to handle dropping a task into a new status lane
+function handleDrop(event, ui) {
+
+}
+
+// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+$(document).ready(function () {
+
+});
